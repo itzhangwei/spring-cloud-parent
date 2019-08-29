7aad0a9c2e9d9f4b54980bc5139fb45c6230081f
zack.zhang <zhangwei>
2019-08-29 19:58:37 +0800

    文件初始化提交

---
+++ config/config-service/src/main/java/com/learn/config/ConfigServiceApplication.java
@@ -0,0 +1,22 @@
+package com.learn.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author zack.zhang
+ * @projectName spring-cloud-parent
+ * @title ConfigServiceApplication
+ * @package com.learn.config
+ * @description 配置中心服务启动类
+ * @date 2019/8/29 7:14 下午
+ */
+@SpringCloudApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
---
+++ eureka/eureka-service/src/main/java/com/learn/EurekaServiceApplication.java
@@ -0,0 +1,23 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zack.zhang
+ * @projectName spring-cloud-parent
+ * @title EurekaServiceApplication
+ * @package com.learn
+ * @description 启动类
+ * @date 2019/8/29 9:45 上午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
