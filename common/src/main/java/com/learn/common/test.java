730041309a9080a007b6d49ab46d1e1da458eaf7
zack.zhang <zhangwei>
2020-08-04 16:27:09 +0800

    测试文件

---
+++ common/src/main/java/com/learn/common/test.java
@@ -0,0 +1,13 @@
+package com.learn.common;
+
+/**
+ * @author zhang
+ * @projectName spring-cloud-parent
+ * @title test
+ * @package com.learn.common
+ * @description
+ * @date 2020/8/4 4:26 下午
+ */
+public class test {
+
+}
